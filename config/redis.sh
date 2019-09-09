cd06c7ce6f10012cc2e2303d9228fff8e25ad95b
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-09 23:41:26 +0900

    Redis 서버 구성

---
+++ config/redis.sh
@@ -0,0 +1,8 @@
+: "${DELTA_DEV_DASHBOARD_REDIS_PASSWORD?DELTA_DEV_DASHBOARD_REDIS_PASSWORD}"
+
+docker run -d \
+-p 10561:6379 \
+--name delta-dev-dashboard-redis \
+--restart=unless-stopped \
+redis:5 \
+--requirepass $DELTA_DEV_DASHBOARD_REDIS_PASSWORD
