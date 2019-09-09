3c70753e09d4e1bf29c8ec9fe2a95e272710ac81
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-10 06:45:31 +0900

    서버를 배포합니다

---
+++ DeltaDevDashboard/build.sh
@@ -0,0 +1,1 @@
+docker build -t delta-dev-dashboard-app-server .
---
+++ DeltaDevDashboard/remote.sh
@@ -0,0 +1,5 @@
+sudo docker load --input docker-image.tar && \
+(sudo docker stop delta-dev-dashboard-app-server || true) && \
+(sudo docker rm delta-dev-dashboard-app-server || true) && \
+sudo docker run -d --restart=unless-stopped \
+--name delta-dev-dashboard-app-server delta-dev-dashboard-app-server
