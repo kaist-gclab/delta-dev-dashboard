9fd24751de7d878d925e8c9517281fb0429ead95
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-10 14:08:59 +0900

    웹 애플리케이션 Dockerize 수행합니다

---
+++ delta-dev-dashboard-web/remote.sh
@@ -0,0 +1,5 @@
+sudo docker load --input docker-image.tar && \
+(sudo docker stop delta-dev-dashboard-web || true) && \
+(sudo docker rm delta-dev-dashboard-web || true) && \
+sudo docker run -d --restart=unless-stopped \
+--name delta-dev-dashboard-web delta-dev-dashboard-web
