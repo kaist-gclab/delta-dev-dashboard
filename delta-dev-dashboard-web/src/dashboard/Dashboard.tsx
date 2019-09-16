629ae42672457d5b5e4d19565a42df54ce53359c
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-09-17 08:06:45 +0900

    인터페이스를 개선합니다

--- delta-dev-dashboard-web/src/dashboard/Dashboard.tsx
+++ delta-dev-dashboard-web/src/dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import DashboardStore from './store';
 import styled from 'styled-components';
-import { Colors, Callout, Tag, H4, Icon } from '@blueprintjs/core';
+import { Colors, Callout, Tag, Icon, H4 } from '@blueprintjs/core';
 import moment from 'moment';
 
 const Interval = 4096;
@@ -33,17 +33,33 @@ const Summary = styled.div`
   }
 `;
 
+const SummaryCardTitle: React.FC = (props) =>
+  <div>
+    <H4>{props.children}</H4>
+  </div>
+
 const SummaryCard = styled(Callout)`
-  & > h4 {
-    color: white;
+  & > div {
+    & > h4 {
+      color: ${Colors.LIGHT_GRAY3};
+    }
+    margin-bottom: 10px;
   }
-  span:not(:last-child) {
-    margin-right: 10px;
+  & > span:not(:last-child) {
+    margin-right: 5px;
     margin-bottom: 5px;
   }
 `;
 
-const SummaryCardElement: React.FC = (props) => <Tag large={true}>{props.children}</Tag>;
+const SummaryCardElementTag = styled(Tag)`
+  & > span {
+    min-width: 125px;
+    text-align: center;
+  }
+`;
+
+const SummaryCardElement: React.FC = (props) =>
+  <SummaryCardElementTag large={true}>{props.children}</SummaryCardElementTag>;
 
 const CurrentInstant = styled.div`
   text-align: right;
@@ -93,18 +109,21 @@ class Dashboard extends React.Component<Props> {
     return <Wrapper>
       <Title>{projectTitle}</Title>
       <Summary>
-        <SummaryCard title="프로젝트 기본 정보" icon="time">
+        <SummaryCard icon="time">
+          <SummaryCardTitle>프로젝트 기본 정보</SummaryCardTitle>
           <SummaryCardElement><Icon icon="flame" /> 시작 {formatDate(projectStartDate)}</SummaryCardElement>
-          <SummaryCardElement><Icon icon="notifications" /> 종료 예상 {formatDate(projectDueDate)}</SummaryCardElement>
-          <SummaryCardElement><Icon icon="calendar" /> 예상 기간 {getTotalDays(projectStartDate, projectDueDate)}일</SummaryCardElement>
+          <SummaryCardElement><Icon icon="notifications" /> 종료 목표 {formatDate(projectDueDate)}</SummaryCardElement>
+          <SummaryCardElement><Icon icon="calendar" /> 계획 기간 {getTotalDays(projectStartDate, projectDueDate)}일</SummaryCardElement>
         </SummaryCard>
-        <SummaryCard title="시작 지표" icon="time">
+        <SummaryCard icon="time">
+          <SummaryCardTitle>시작 지표</SummaryCardTitle>
           <SummaryCardElement><Icon icon="git-repo" /> 저장소 {gitHubTargetsBegin.repositories}개</SummaryCardElement>
           <SummaryCardElement><Icon icon="git-commit" /> 커밋 {gitHubTargetsBegin.commits}개</SummaryCardElement>
           <SummaryCardElement><Icon icon="star" /> 스타 {gitHubTargetsBegin.stargazers}개</SummaryCardElement>
           <SummaryCardElement><Icon icon="people" /> 기여자 {gitHubTargetsBegin.contributors}명</SummaryCardElement>
         </SummaryCard>
-        <SummaryCard title="목표 지표" icon="time">
+        <SummaryCard icon="time">
+          <SummaryCardTitle>목표 지표</SummaryCardTitle>
           <SummaryCardElement><Icon icon="git-repo" /> 저장소 {gitHubTargetsEnd.repositories}개</SummaryCardElement>
           <SummaryCardElement><Icon icon="git-commit" /> 커밋 {gitHubTargetsEnd.commits}개</SummaryCardElement>
           <SummaryCardElement><Icon icon="star" /> 스타 {gitHubTargetsEnd.stargazers}개</SummaryCardElement>
