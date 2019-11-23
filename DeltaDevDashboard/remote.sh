2b25c56f7560dac342fb082e46d91ce70d892bdf
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-23 16:52:53 +0900

    배포 완료 후 불필요한 파일을 삭제하도록 합니다

--- DeltaDevDashboard/remote.sh
+++ DeltaDevDashboard/remote.sh
@@ -2,4 +2,5 @@ sudo docker load --input docker-image.tar && \
 (sudo docker stop delta-dev-dashboard-app-server || true) && \
 (sudo docker rm delta-dev-dashboard-app-server || true) && \
 sudo docker run -d --restart=unless-stopped \
---name delta-dev-dashboard-app-server delta-dev-dashboard-app-server
+--name delta-dev-dashboard-app-server delta-dev-dashboard-app-server && \
+rm docker-image.tar
--- delta-dev-dashboard-web/remote.sh
+++ delta-dev-dashboard-web/remote.sh
@@ -2,4 +2,5 @@ sudo docker load --input docker-image.tar && \
 (sudo docker stop delta-dev-dashboard-web || true) && \
 (sudo docker rm delta-dev-dashboard-web || true) && \
 sudo docker run -d --restart=unless-stopped \
---name delta-dev-dashboard-web delta-dev-dashboard-web
+--name delta-dev-dashboard-web delta-dev-dashboard-web && \
+rm docker-image.tar
