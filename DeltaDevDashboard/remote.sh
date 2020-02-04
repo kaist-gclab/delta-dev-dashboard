1f630a5f4d9527d8f6ac18571903e4b9b10d1bac
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-02-04 15:38:40 +0900

    불필요한 sudo를 제거합니다

--- DeltaDevDashboard/remote.sh
+++ DeltaDevDashboard/remote.sh
@@ -3,10 +3,10 @@
 : "${DELTA_DEV_DASHBOARD_REDIS_HOST?DELTA_DEV_DASHBOARD_REDIS_HOST}"
 : "${DELTA_DEV_DASHBOARD_REDIS_PORT?DELTA_DEV_DASHBOARD_REDIS_PORT}"
 
-sudo docker load --input docker-image.tar && \
-(sudo docker stop delta-dev-dashboard-app-server || true) && \
-(sudo docker rm delta-dev-dashboard-app-server || true) && \
-sudo docker run -d --restart=unless-stopped \
+docker load --input docker-image.tar && \
+(docker stop delta-dev-dashboard-app-server || true) && \
+(docker rm delta-dev-dashboard-app-server || true) && \
+docker run -d --restart=unless-stopped \
 -e Redis__Password=$DELTA_DEV_DASHBOARD_REDIS_PASSWORD \
 -e Redis__Hosts__0__Host=$DELTA_DEV_DASHBOARD_REDIS_HOST \
 -e Redis__Hosts__0__Port=$DELTA_DEV_DASHBOARD_REDIS_PORT \
--- delta-dev-dashboard-web/remote.sh
+++ delta-dev-dashboard-web/remote.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo docker load --input docker-image.tar && \
-(sudo docker stop delta-dev-dashboard-web || true) && \
-(sudo docker rm delta-dev-dashboard-web || true) && \
-sudo docker run -d --restart=unless-stopped \
+docker load --input docker-image.tar && \
+(docker stop delta-dev-dashboard-web || true) && \
+(docker rm delta-dev-dashboard-web || true) && \
+docker run -d --restart=unless-stopped \
 --name delta-dev-dashboard-web delta-dev-dashboard-web && \
 rm docker-image.tar
